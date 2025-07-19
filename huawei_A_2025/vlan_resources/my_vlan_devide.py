8ab9c149dc15d136da244dacb06cc6853ca12904
Teyeray <1048423879@qq.com>
2025-07-19 23:06:38 +0800

    bug fixed

--- huawei_A_2025/vlan_resources/my_vlan_devide.py
+++ huawei_A_2025/vlan_resources/my_vlan_devide.py
@@ -1,8 +1,15 @@
+'''
+67-899,45,3-44,1
+898
+1,3-45,67-897,899
+'''
 #line = input().strip()
 
 #line = '20-21,15,18,30,5-10'
-line = '1-5'
-vlan = '2'
+line = '67-899,45,3-44,1'
+vlan = '898'
+# line = '1-10'
+# vlan = '9'
 
 #print(f'line: {line},line_type: {type(line)}')
 
@@ -25,15 +32,15 @@ def parse_ranges(input_line):
 def re_format(input_lines):
     result = []
     i = 0
-    while i < len(input_lines) - 1:
+    while i < len(input_lines):
         curr = input_lines[i]
-        if input_lines[i] + 1 == input_lines[i + 1]:
+        if i != len(input_lines) - 1 and input_lines[i] + 1 == input_lines[i + 1]:
             while i < len(input_lines) - 1 and input_lines[i] + 1 == input_lines[i + 1]:
                 curr_end = input_lines[i + 1]
                 i += 1
             result.append(f'{curr}-{curr_end}')
         else:
-            result.append(str(curr))
+            result.append(str(input_lines[i]))
         i += 1
     print(f're_format result: {result}\n')
     return ','.join(result)
