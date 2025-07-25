2786835391cd1032f63650c0c1feb73073313198
Teyeray <1048423879@qq.com>
2025-07-25 11:25:43 +0800

    new file

---
+++ 1665.minimum-initial-energy-to-finish-tasks.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=1665 lang=python3
+#
+# [1665] Minimum Initial Energy to Finish Tasks
+#
+
+# @lc code=start
+class Solution:
+    def minimumEffort(self, tasks: List[List[int]]) -> int:
+        
+# @lc code=end
+
