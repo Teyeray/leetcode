cc823ea112ca175da57a0dd5d12d0003e0ba7b26
Teyeray <1048423879@qq.com>
2025-07-25 13:24:35 +0800

    update

---
+++ 剑指offer/JZ3.数组中重复的数字.py
@@ -0,0 +1,19 @@
+# @nc app=nowcoder id=6fe361ede7e54db1b84adc81d09d8524 topic=13 question=11203 lang=Python3
+# 2025-07-25 12:00:54
+# https://www.nowcoder.com/practice/6fe361ede7e54db1b84adc81d09d8524?tpId=13&tqId=11203
+# [JZ3] 数组中重复的数字
+
+# @nc code=start
+
+#
+# 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+#
+# 
+# @param numbers int整型一维数组 
+# @return int整型
+#
+class Solution:
+    def duplicate(self , numbers: List[int]) -> int:
+        # write code here
+
+# @nc code=end
