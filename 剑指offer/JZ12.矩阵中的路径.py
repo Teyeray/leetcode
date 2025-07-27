216636211e8565b92c0d9c8d147d25de2353c734
Teyeray <1048423879@qq.com>
2025-07-27 23:42:49 +0800

    dfs寻找路径

--- 剑指offer/JZ12.矩阵中的路径.py
+++ 剑指offer/JZ12.矩阵中的路径.py
@@ -16,5 +16,25 @@
 class Solution:
     def hasPath(self , matrix: List[List[str]], word: str) -> bool:
         # write code here
-
+        rows, cols = len(matrix), len(matrix[0])
+        def dfs(r, c, k):
+            if r < 0 or r >= rows or c < 0 or c >= cols or matrix[r][c] != word[k]:
+                return False
+            if k == len(word) - 1:
+                return True      
+            tmp = matrix[r][c]
+            matrix[r][c] = '#'
+            res = (
+                dfs(r + 1, c, k + 1) or
+                dfs(r, c + 1, k + 1) or
+                dfs(r - 1, c, k + 1) or
+                dfs(r, c - 1, k + 1)
+            )
+            matrix[r][c] = tmp
+            return res
+        for i in range(rows):
+            for j in range(cols):
+                if dfs(i, j, 0):
+                    return True
+        return False
 # @nc code=end
