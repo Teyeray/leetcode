ade97f7b17f0ef9715b6eb8ac8efb093e1c32312
Teyeray <1048423879@qq.com>
2025-10-19 21:07:50 +0800

    new

---
+++ 剑指offer/JZ35.复杂链表的复制.py
@@ -0,0 +1,20 @@
+# @nc app=nowcoder id=f836b2c43afc4b35ad6adc41ec941dba topic=13 question=11178 lang=Python3
+# 2025-10-19 20:48:29
+# https://www.nowcoder.com/practice/f836b2c43afc4b35ad6adc41ec941dba?tpId=13&tqId=11178
+# [JZ35] 复杂链表的复制
+
+# @nc code=start
+
+# -*- coding:utf-8 -*-
+# class RandomListNode:
+#     def __init__(self, x):
+#         self.label = x
+#         self.next = None
+#         self.random = None
+class Solution:
+    # 返回 RandomListNode
+    def Clone(self, pHead):
+        # write code here
+
+
+# @nc code=end
--- 剑指offer/JZ7.重建二叉树.py
+++ 剑指offer/JZ7.重建二叉树.py
@@ -5,6 +5,7 @@
 
 # @nc code=start
 
+
 # class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
@@ -13,13 +14,15 @@
 #
 # 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
 #
-# 
-# @param preOrder int整型一维数组 
-# @param vinOrder int整型一维数组 
+#
+# @param preOrder int整型一维数组
+# @param vinOrder int整型一维数组
 # @return TreeNode类
 #
 class Solution:
-    def reConstructBinaryTree(self , preOrder: List[int], vinOrder: List[int]) -> TreeNode:
+    def reConstructBinaryTree(
+        self, preOrder: List[int], vinOrder: List[int]
+    ) -> TreeNode:
         # write code here
         if not preOrder:
             return None
@@ -27,10 +30,11 @@ class Solution:
         root = TreeNode(root_val)
         idx = vinOrder.index(root_val)
 
-        root.left = self.reConstructBinaryTree(preOrder[1 : 1 + idx], vinOrder[0 : idx])
-        root.right = self.reConstructBinaryTree(preOrder[1 + idx :], vinOrder[idx + 1 :])
+        root.left = self.reConstructBinaryTree(preOrder[1 : 1 + idx], vinOrder[0:idx])
+        root.right = self.reConstructBinaryTree(
+            preOrder[1 + idx :], vinOrder[idx + 1 :]
+        )
         return root
 
 
-        
 # @nc code=end
